2e5d0b6dd3f6cf9036f97162dcad0af50ef3fd5a
Jonathan Gin <jonathangin52@gmail.com>
2021-12-01 01:20:00 -0500

    Run gofmt

--- day1/main.go
+++ day1/main.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "strings"
-  "strconv"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-  rawData, _ := os.ReadFile("./day1/input.txt")
-  data := strings.Split(string(rawData), "\n")
+	rawData, _ := os.ReadFile("./day1/input.txt")
+	data := strings.Split(string(rawData), "\n")
 
-  fmt.Println(part1(data))
-  fmt.Println(part2(data))
+	fmt.Println(part1(data))
+	fmt.Println(part2(data))
 }
 
 func part1(data []string) int {
-  count := 0
-  prev, _ := strconv.Atoi(data[0])
+	count := 0
+	prev, _ := strconv.Atoi(data[0])
 
-  for _, val := range data[1:] {
-    curr, _ := strconv.Atoi(val)
+	for _, val := range data[1:] {
+		curr, _ := strconv.Atoi(val)
 
-    if curr > prev {
-      count++
-    }
+		if curr > prev {
+			count++
+		}
 
-    prev = curr
-  }
+		prev = curr
+	}
 
-  return count
+	return count
 }
 
 func part2(data []string) int {
-  count := 0
-  prevSum := -1
+	count := 0
+	prevSum := -1
 
-  for i := 1; i < len(data) - 1; i++ {
-    prev, _ := strconv.Atoi(data[i - 1])
-    curr, _ := strconv.Atoi(data[i])
-    next, _ := strconv.Atoi(data[i + 1])
+	for i := 1; i < len(data)-1; i++ {
+		prev, _ := strconv.Atoi(data[i-1])
+		curr, _ := strconv.Atoi(data[i])
+		next, _ := strconv.Atoi(data[i+1])
 
-    sum := prev + curr + next
+		sum := prev + curr + next
 
-    if sum > prevSum && prevSum != -1 {
-      count++
-    }
+		if sum > prevSum && prevSum != -1 {
+			count++
+		}
 
-    prevSum = sum
-  }
+		prevSum = sum
+	}
 
-  return count
+	return count
 }
