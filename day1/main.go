fee16a82479a567815ee6700a29723fcaf3088d0
Jonathan Gin <jonathangin52@gmail.com>
2021-12-01 01:15:00 -0500

    Day 1 - Part 2

--- day1/main.go
+++ day1/main.go
@@ -12,6 +12,7 @@ func main() {
   data := strings.Split(string(rawData), "\n")
 
   fmt.Println(part1(data))
+  fmt.Println(part2(data))
 }
 
 func part1(data []string) int {
@@ -26,6 +27,27 @@ func part1(data []string) int {
     }
 
     prev = curr
+  }
+
+  return count
+}
+
+func part2(data []string) int {
+  count := 0
+  prevSum := -1
+
+  for i := 1; i < len(data) - 1; i++ {
+    prev, _ := strconv.Atoi(data[i - 1])
+    curr, _ := strconv.Atoi(data[i])
+    next, _ := strconv.Atoi(data[i + 1])
+
+    sum := prev + curr + next
+
+    if sum > prevSum && prevSum != -1 {
+      count++
+    }
+
+    prevSum = sum
   }
 
   return count
