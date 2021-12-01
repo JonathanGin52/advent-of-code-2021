91eab7d8a2b0c42fa024556f90fbc59a514de890
Jonathan Gin <jonathangin52@gmail.com>
2021-12-01 01:15:00 -0500

    Day 2 - Part 1

---
+++ day2/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	rawData, _ := os.ReadFile("./day2/input.txt")
+	data := strings.Split(strings.TrimSpace(string(rawData)), "\n")
+
+	fmt.Println(part1(data))
+}
+
+func part1(data []string) int {
+	horizontal := 0
+	depth := 0
+
+	for _, command := range data {
+		split := strings.Split(command, " ")
+		direction := split[0]
+		magnitude, _ := strconv.Atoi(split[1])
+
+		if direction == "forward" {
+			horizontal += magnitude
+		} else if direction == "up" {
+			depth -= magnitude
+		} else if direction == "down" {
+			depth += magnitude
+		} else {
+			panic("Invalid direction")
+		}
+	}
+
+	return horizontal * depth
+}
