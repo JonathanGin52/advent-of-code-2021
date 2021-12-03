d1630bbffb072c1ecda073af587e1ff3eb952573
Jonathan Gin <jonathangin52@gmail.com>
2021-12-03 00:29:17 -0500

    Day 2 - Part 2

--- day2/main.go
+++ day2/main.go
@@ -12,6 +12,7 @@ func main() {
 	data := strings.Split(strings.TrimSpace(string(rawData)), "\n")
 
 	fmt.Println(part1(data))
+	fmt.Println(part2(data))
 }
 
 func part1(data []string) int {
@@ -29,6 +30,31 @@ func part1(data []string) int {
 			depth -= magnitude
 		} else if direction == "down" {
 			depth += magnitude
+		} else {
+			panic("Invalid direction")
+		}
+	}
+
+	return horizontal * depth
+}
+
+func part2(data []string) int {
+	horizontal := 0
+	depth := 0
+	aim := 0
+
+	for _, command := range data {
+		split := strings.Split(command, " ")
+		direction := split[0]
+		magnitude, _ := strconv.Atoi(split[1])
+
+		if direction == "forward" {
+			horizontal += magnitude
+			depth += aim * magnitude
+		} else if direction == "up" {
+			aim -= magnitude
+		} else if direction == "down" {
+			aim += magnitude
 		} else {
 			panic("Invalid direction")
 		}
