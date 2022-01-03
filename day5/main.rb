595ec1b2fc658828ea8965ba6c955125f6cfc72b
Jonathan Gin <jonathangin52@gmail.com>
2022-01-03 16:54:00 +0500

    Day 5 - Part 2

--- day5/main.rb
+++ day5/main.rb
@@ -8,16 +8,32 @@ File.readlines('input.txt').each do |line|
 
   x1, y1 = start.split(',').map(&:to_i)
   x2, y2 = dest.split(',').map(&:to_i)
-  xs = [x1, x2]
-  ys = [y1, y2]
 
-  next unless x1 == x2 || y1 == y2
+  if x1 == x2 || y1 == y2
+    # Horizontal and vertical lines
+    xs = [x1, x2]
+    ys = [y1, y2]
+    (xs.min..xs.max).each do |x|
+      (ys.min..ys.max).each do |y|
+        map[x][y] += 1
 
-  (xs.min..xs.max).each do |x|
-    (ys.min..ys.max).each do |y|
+        count += 1 if map[x][y] == 2
+      end
+    end
+  elsif (y2 - y1).abs == (x2 - x1).abs
+    # Diagonal lines
+    x = x1
+    y = y1
+    x_incr = x1 < x2 ? 1 : -1
+    y_incr = y1 < y2 ? 1 : -1
+
+    loop do
       map[x][y] += 1
-
       count += 1 if map[x][y] == 2
+      break if x == x2 && y == y2
+
+      x += x_incr
+      y += y_incr
     end
   end
 end
