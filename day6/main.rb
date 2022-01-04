09ef619ec0448e441361e25a6af6c956126cdbf1
Jonathan Gin <jonathangin52@gmail.com>
2022-01-04 17:03:00 +0500

    Day 6 - Improve method

--- day6/main.rb
+++ day6/main.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
-fish = File.read('input.txt').split(',').map(&:to_i)
+data = File.read('input.txt').split(',').map(&:to_i)
 days = 80
 
+fish = Array.new(9, 0)
+data.each { |timer| fish[timer] += 1 }
+
 days.times do
-  new_fish_count = 0
-  fish.each_index do |i|
-    fish[i] -= 1
-    if fish[i] < 0
-      fish[i] = 6
-      new_fish_count += 1
-    end
-  end
-  new_fish_count.times { fish << 8 }
+  new_fish = fish[0]
+  (0..7).each { |i| fish[i] = fish[i + 1] }
+  fish[6] += new_fish
+  fish[8] = new_fish
 end
 
-puts fish.size
+puts fish.sum
