fb2376c7041c40fc226cf6e0662addb023536e2b
Jonathan Gin <jonathangin52@gmail.com>
2022-01-04 20:25:00 +0500

    Day 7 - Part 2

--- day7/main.rb
+++ day7/main.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
 positions = File.read('input.txt').split(',').map(&:to_i).sort
-target = positions[positions.size / 2]
-puts positions.sum { |loc| (loc - target).abs }
+
+min_sum = Float::INFINITY
+positions.min.upto(positions.max).each do |target|
+  sum = positions.sum do |loc|
+    delta = (loc - target).abs
+    delta * (delta + 1) / 2
+  end
+  min_sum = [sum, min_sum].min
+end
+
+puts min_sum
